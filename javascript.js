f72e00da20c3d0430554015271e157c20f1edc86
thejesterformerlyknownasyorick <42towels@gmail.com>
2022-09-19 12:29:02 -0700

    modified sizing to allow grid of all sizes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,20 @@
-const container = document.querySelector('.container');
+let size;
 
-for(i = 1; i < 273; i++) {
-    const box = document.createElement('div');
-    box.className = "box";
+const container = document.querySelector('.container');
+const controlPanel = document.createElement('div');
+const sizeButton = document.createElement('button');
+const body = document.querySelector('body');
+
+controlPanel.className = "panel";
+body.appendChild(controlPanel);
+sizeButton.textContent = "How many squares (squared) ya want in this thing?";
+controlPanel.appendChild(sizeButton);
+
+size = parseInt(prompt("How many squares (squared) ya want?", "16"));
+
+for(i = 0; i < size * size; i++) {
+    const box = document.createElement('div'); 
+    box.className = "box";    
     container.appendChild(box);
     box.addEventListener('mouseover', function(e) {
         box.classList.add('hover');
@@ -12,4 +24,10 @@ for(i = 1; i < 273; i++) {
         box.classList.add('leave');
         box.classList.remove('hover');
     });
+    box.style.height = `${800/size}px`;
+    box.style.width = `${800/size}px`;
 }
+
+sizeButton.addEventListener('click', function() {
+    location.reload();
+});
