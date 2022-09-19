f9565572f5a03949802a1c01620b07cfaaa437f6
thejesterformerlyknownasyorick <42towels@gmail.com>
2022-09-19 13:36:54 -0700

    add title and click functionality

--- javascript.js
+++ javascript.js
@@ -11,21 +11,31 @@ sizeButton.textContent = "How many squares (squared) ya want in this thing?";
 controlPanel.appendChild(sizeButton);
 
 size = parseInt(prompt("How many squares (squared) ya want?", "16"));
+    if (size > 100) {
+        window.alert("That's too much, man! Try something more in the range of 1 to 100.\n\nIt's not that I can't make it happen. It's just that your computer is too weak.");
+    }
 
-for(i = 0; i < size * size; i++) {
-    const box = document.createElement('div'); 
-    box.className = "box";    
-    container.appendChild(box);
-    box.addEventListener('mouseover', function(e) {
-        box.classList.add('hover');
-        box.classList.remove('leave');
-    });
-    box.addEventListener('mouseleave', function(e) {
-        box.classList.add('leave');
-        box.classList.remove('hover');
-    });
-    box.style.height = `${800/size}px`;
-    box.style.width = `${800/size}px`;
+    else {
+        for(i = 0; i < size * size; i++) {
+        const box = document.createElement('div'); 
+        box.className = "box";    
+        container.appendChild(box);
+        box.addEventListener('mouseover', function(e) {
+            box.classList.add('hover');
+            box.classList.remove('leave');
+        });
+        box.addEventListener('mouseleave', function(e) {
+            box.classList.add('leave');
+            box.classList.remove('hover');
+        });
+        box.addEventListener('click', function(e) {
+            box.classList.remove('leave');
+            box.classList.remove('hover');
+            box.classList.add('dark');
+        }, {once: true});
+        box.style.height = `${800/size}px`;
+        box.style.width = `${800/size}px`;
+    }
 }
 
 sizeButton.addEventListener('click', function() {
